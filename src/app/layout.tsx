e483ba58b1416e3dd422ce4b8b5b1ee03c5912c7
Jong1co <narira0922@gmail.com>
2023-04-21 10:45:33 +0900

    [feature] home page layout 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,17 @@
-// import { Header } from "@/_common/Header";
-import "./globals.css";
-import { Footer } from "@/_common/Footer";
+import { Header } from '@/_common/Header';
+import './globals.css';
+import { Footer } from '@/_common/Footer';
 
 export const metadata = {
-  title: "Jonghyun blog",
-  description: "",
+  title: 'Jonghyun blog',
+  description: '',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        {/* <Header /> */}
-        <header className="bg-gray-500">hello</header>
+    <html lang="en" className="flex justify-center w-full bg-neutral-20">
+      <body className="relative w-full max-w-3xl min-h-screen ">
+        <Header />
         <main>{children}</main>
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { Banner } from "@/_common/Banner";
+import { Banner } from '@/_common/Banner';
+import { RecentPosts } from './components/RecentPosts';
 
 export default function Home() {
   return (
     <section>
       <Banner />
+      <RecentPosts />
     </section>
   );
 }
