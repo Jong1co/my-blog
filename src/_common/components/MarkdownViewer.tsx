5da2ba304c304556e69ec225baa96bb1a8006bd6
Jong1co <narira0922@gmail.com>
2023-05-13 00:20:38 +0900

    [fix] react-markdown의 code 태그 양 측에 백틱(`)이 나타나는 현상 수정

--- src/_common/components/MarkdownViewer.tsx
+++ src/_common/components/MarkdownViewer.tsx
@@ -26,12 +26,14 @@ export const MarkdownViewer = ({ content }: { content: string }) => {
                 {String(children).replace(/\n$/, '')}
               </SyntaxHighlighter>
             ) : (
-              <code {...props} className="p-1 mr-1 rounded-md bg-neutral-50 text-neutral-100 ">
+              <span {...props} className="px-2 py-1 mr-1 rounded-md bg-neutral-50 text-neutral-100 ">
                 {children}
-              </code>
+              </span>
             );
           },
-          img: (props) => <Image src={props.src || ''} alt={props.alt || ''} width={500} height={300} />,
+          img: (props) => (
+            <Image src={props.src || ''} alt={props.alt || ''} width={500} height={300} className="object-cover w-full max-h-60" />
+          ),
           h1: (props) => <h1 className="text-neutral-100 ft-header-01">{props.children}</h1>,
           h2: (props) => <h2 className="text-neutral-100 ft-header-02">{props.children}</h2>,
           h3: (props) => <h3 className="text-neutral-100 ft-header-03">{props.children}</h3>,
