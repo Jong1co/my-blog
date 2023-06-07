c8763b5d33f7d59c7a36b5005ffd9b54dba4d134
jonghyun <95389265+Jong1co@users.noreply.github.com>
2023-06-07 20:49:07 +0900

    Release (#8)
    * release v1.1.0 (#5)
    
    * [feature] utterances로 댓글 기능 구현 (github issues와 연결)
    
    * [feature] modal이 켜져있을 경우 스크롤 방지하는 커스텀훅 생성
    
    * merge (#7)
    
    * release v1.1.0 (#5) (#6)
    
    * [feature] utterances로 댓글 기능 구현 (github issues와 연결)
    
    * [feature] modal이 켜져있을 경우 스크롤 방지하는 커스텀훅 생성
    
    * Update page.tsx
    
    * 게시글 추가

--- src/_common/components/Banner.tsx
+++ src/_common/components/Banner.tsx
@@ -3,7 +3,6 @@ import { MyInfo } from './MyInfo';
 
 export const Banner = async () => {
   const category = await getAllCategories();
-  console.log(category);
 
   return (
     <section className="flex flex-col items-center justify-start w-full max-w-2xl px-4 pb-10 mx-auto whitespace-pre ">
